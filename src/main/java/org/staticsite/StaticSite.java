dfcb758d97a78d3ad85a2d2042f8c6bd32b34559
Denis Borodin <borocopy@gmail.com>
2022-12-05 23:06:45 +0300

    Only md files are parsed

--- src/main/java/org/staticsite/StaticSite.java
+++ src/main/java/org/staticsite/StaticSite.java
@@ -1,10 +1,20 @@
 package org.staticsite;
 
+import org.apache.commons.io.FilenameUtils;
 import org.staticsite.parser.MarkdownParser;
 import picocli.CommandLine;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+
+import javax.annotation.processing.Filer;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Command(
     name = "sitegen",
@@ -23,8 +33,24 @@ public class StaticSite implements Runnable {
 
     @Override
     public void run() {
-        parser.parse(rootDirPath);
-        System.out.println("Site has been successfully generated!");
+        try {
+            List<File> filesInFolder = Files.walk(Paths.get(rootDirPath))
+                    .filter(Files::isRegularFile)
+                    .filter(
+                        (path) -> FilenameUtils.getExtension(path.toString()).equals("md")
+                    )
+                    .map(Path::toFile)
+                    .collect(Collectors.toList());
+            for (File file: filesInFolder) {
+                parser.parse(file.getAbsolutePath());
+                System.out.println(
+                    "File " + file.getName() + " has been processed."
+                );
+            }
+            System.out.println("Site has been successfully generated!");
+        } catch(IOException error) {
+           System.err.println(error.getMessage());
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/staticsite/entity/Article.java
@@ -0,0 +1,49 @@
+package org.staticsite.entity;
+
+import java.util.Date;
+
+public class Article {
+    private String author;
+    private String title;
+    private Date dateWritten;
+    private String content;
+
+    public Article(String author, String title, Date dateWritten, String content) {
+        this.author = author;
+        this.title = title;
+        this.dateWritten = dateWritten;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDateWritten() {
+        return dateWritten;
+    }
+
+    public void setDateWritten(Date dateWritten) {
+        this.dateWritten = dateWritten;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
